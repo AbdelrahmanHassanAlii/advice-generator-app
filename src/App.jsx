24ecc216045ee5bbedb8e1c13dd779c4506512b3
venom <abdo30201080101694@gmail.com>
2024-09-08 22:05:01 +0300

    add my links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
 import { Advice } from "./components/Advice";
 
+import "./App.css";
+
 function App() {
-  return <Advice />;
+  return (
+    <div className="App">
+      <Advice />
+      <h1 className="attribution">
+        Responsive Advice Generator by{" "}
+        <Link
+          to="https://abdelrahmanhassanalii.github.io/card/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Abderahman Hassan
+        </Link>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
